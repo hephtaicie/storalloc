72f8d22ae05c8943e3e8edf282240b950b07b839
MONNIOT Julien <julien.monniot@inria.fr>
2021-10-27 15:34:17 +0200

    LInt on config file, but some naming conventions still seem a bit off...

--- src/storalloc/config_file.py
+++ src/storalloc/config_file.py
@@ -1,59 +1,54 @@
-#!/usr/bin/env python3
+""" Storalloc
+    ConfigFile parser
+"""
 
 import os
 import sys
 import yaml
-import io
-from pathlib import Path
 
 
-class ConfigFile(object):
+class ConfigFile:
+    """Representation of a config file, parsed from YAML file"""
+
     def __init__(self, path):
-        super().__init__()
+        """Init config from file path"""
 
-        self._content = None
-        self._path = path
-
-        if os.path.exists(self._path) and os.access(self._path, os.R_OK):
-            self._parse_content()
+        if os.path.exists(path) and os.access(path, os.R_OK):
+            with open(path, "r", encoding="utf-8") as yaml_stream:
+                self.content = yaml.safe_load(yaml_stream)
         else:
-            print(
-                "Error: the StorAlloc configuration file does not exist or is not readable ("
-                + self._path
-                + ")"
-            )
+            print(f"Error: provided path for configuration seems to be invalid :{path}")
             sys.exit(1)
 
-    def _parse_content(self):
-        stream = open(self._path, "r")
-        self._content = yaml.safe_load(stream)
-        stream.close()
-
-        return self._content
-
-    def get(self):
-        return self._content
-
+    # TODO : some of those config names are rather misleading (what do they point to exactly ?)
     def get_orch_hostname(self):
-        return self._content["orchestrator"]["hostname"]
+        """Get orchestrator hostname"""
+        return self.content["orchestrator"]["hostname"]
 
     def get_orch_ipv4(self):
-        return self._content["orchestrator"]["ipv4"]
+        """Get orchestrator IPv4 address"""
+        return self.content["orchestrator"]["ipv4"]
 
     def get_orch_port(self):
-        return int(self._content["orchestrator"]["port"])
+        """Get orchestrator linstening port"""
+        return int(self.content["orchestrator"]["port"])
 
     def get_orch_strategy(self):
-        return self._content["orchestrator"]["sched_strategy"]
+        """Get orchestrator strategy"""
+        return self.content["orchestrator"]["sched_strategy"]
 
     def get_orch_client_bind_ipv4(self):
-        return self._content["orchestrator"]["client_bind"]["ipv4"]
+        """Get orchestrator client hostname"""
+        return self.content["orchestrator"]["client_bind"]["ipv4"]
 
     def get_orch_client_bind_port(self):
-        return self._content["orchestrator"]["client_bind"]["port"]
+        """Get orchestrator client binding port"""
+        return self.content["orchestrator"]["client_bind"]["port"]
 
     def get_orch_server_bind_ipv4(self):
-        return self._content["orchestrator"]["server_bind"]["ipv4"]
+        """Get orchestrator server hostname"""
+        return self.content["orchestrator"]["server_bind"]["ipv4"]
 
     def get_orch_server_bind_port(self):
-        return self._content["orchestrator"]["server_bind"]["port"]
+        """Get orchestrator server binding port"""
+        return self.content["orchestrator"]["server_bind"]["port"]
