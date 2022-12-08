142c1e33b9c868f6037442dff3f3192853229b9d
MONNIOT Julien <julien.monniot@inria.fr>
2022-12-08 22:19:58 +0100

    Rework of worst-case algorithm, to simplify computations and fix a bug (with hopes of preserving more or less the same behaviour as before)

--- src/storalloc/strategies/worst_case.py
+++ src/storalloc/strategies/worst_case.py
@@ -15,40 +15,48 @@ class WorstCase(StrategyInterface):
     def compute(self, resource_catalog, request):
         """Compute worst case allocation"""
 
+        self.log.debug(f"[WORST_CASE] Received NEW request {request}")
+
+        # Update resource catalog (status of disks and nodes) according to the new req
         self.__compute_status(resource_catalog, request)
-        # resources_status.sort(key=lambda x: x.bw, reverse=True)
 
         candidates = []
 
         # Select best disks from every node
         for server_id, node in resource_catalog.list_nodes():
 
-            self.log.debug(f"[WCc] Analysing candidates for node {node.uid}")
+            self.log.debug(f"[WC:compute] Looking for candidates in NODE::{node.uid}")
 
-            self.log.debug(f"[WCc] Disks before filtering: {len(node.disks)} candidates")
+            self.log.debug(f"[WC:compute] {len(node.disks)} disks before filtering")
             filtered_disks = [
                 disk for disk in node.disks if disk.disk_status.capacity >= request.capacity
             ]
-            self.log.debug(f"[WCc] Disks after filtering: {len(filtered_disks)} candidates")
+            self.log.debug(f"[WC:compute] {len(filtered_disks)} disks after filtering")
             # Add every not filtered out disk to the candidates
             candidates.extend([(server_id, node, disk) for disk in filtered_disks])
 
         if not candidates:
             self.log.debug(
-                f"Not enough space on any of the disks, cannot grant request {request.job_id}"
+                "[WC:compute] Not enough space on any of the disks,"
+                + f"cannot grant request {request.job_id}"
             )
             return ("", -1, -1)
 
+        # Sort candidates and pick the best
         sorted_disks = sorted(candidates, key=lambda t: -t[2].disk_status.bandwidth)
         best_bandwidth = sorted_disks[0][2].disk_status.bandwidth
-        self.log.debug(f"[WCc] Best bandwidth among disks for this node : {best_bandwidth}")
+        scd_best_bandwidth = sorted_disks[1][2].disk_status.bandwidth
+        self.log.debug(f"[WC:compute] Best bandwidth among disks for this node : {best_bandwidth}")
+        self.log.debug(f"[WC:compute] 2nd best for this node : {scd_best_bandwidth}")
         final_choices = []
         for server_id, node, disk in sorted_disks:
             if disk.disk_status.bandwidth < best_bandwidth:
                 break
             final_choices.append((server_id, node, disk))
 
-        self.log.debug(f"There are {len(final_choices)} final candidate(s) to choose from")
+        self.log.debug(
+            f"[WC:compute] There are {len(final_choices)} final candidate(s) to choose from"
+        )
         choice = random.choice(final_choices)
         return (choice[0], choice[1].uid, choice[2].uid)
 
@@ -64,108 +72,106 @@ class WorstCase(StrategyInterface):
 
         for server_id, node in resource_catalog.list_nodes():
 
-            node_bw = 0.0
-
-            for disk in node.disks:
-
-                self.log.debug(f"[WC] Analysing disk {server_id}:{node.uid}:{disk.uid}")
-
-                # Update worst case bandwidth for current disk based on possibly
-                # concurrent allocations
-                overlap_offset, tmp_node_bw, disk_bw = self.__compute_allocation_overlap(
-                    disk, node, request
-                )
-                node_bw += tmp_node_bw
-
-                node_bw += (end_time_chunk - (start_time_chunk + overlap_offset)) * node.bandwidth
-                disk_bw += (
-                    end_time_chunk - (start_time_chunk + overlap_offset)
-                ) * disk.write_bandwidth
-                try:
-                    disk_bw = disk_bw / ((request.end_time - request.start_time).seconds)
-                except ZeroDivisionError:
-                    self.log.error(
-                        f"Request {request.job_id} seems to have the same start and end_time..."
-                    )
-
-                self.log.debug(f"[WC] .. Disk/Current node_bw: {node_bw}")
-                self.log.debug(f"[WC] .. Disk/Current disk_bw: {disk_bw}")
-
-                disk.disk_status.bandwidth = disk_bw
-                self.log.debug(
-                    "[WC] .. Access bandwidth and max avail. capacity for disk "
-                    + f"{server_id}:{node.uid}:{disk.uid}"
-                    + f" => {disk.capacity} GB / {disk_bw} GB/s"
-                )
-
-            try:
-                node.node_status.bandwidth = (
-                    node_bw / (request.end_time - request.start_time).seconds / len(node.disks)
-                )
-            except ZeroDivisionError:
-                self.log.error(
-                    f"Request {request.job_id} seems to have the same start and end_time..."
-                )
+            self.__compute_worst_node_disk_bandwidth(node, request)
 
             self.log.debug(
                 f"[WC] .. Access bandwidth for {server_id}:{node.uid}"
-                + f"= {node.node_status.bandwidth} GB/s"
+                + f"= {node.node_status.bandwidth} GB/s per disk"
             )
 
-    def __compute_allocation_overlap(self, disk, node, request):
-        """For a given disk, loop through existing allocations and compute a
-        worst case achievable bandwidth for our new request.
+    def __compute_worst_node_disk_bandwidth(self, node, request):
+        """Compute worst case bw for node and disks"""
 
-        - Only the allocations that end AFTER our new requests starts are considered.
-        - The overlap time between an existing allocation and our new request is exact,
-          but the max number of overlapping requests is a worst case scenario
+        disks_overlaps = []
 
-        Returns the overlap_offset, which can be used to compute how long the new
-        request will possibly run without overlaps.
-        Returns temporary node and disk 'bandwidth' (or rather the amount of
-        bytes that could possibly get through during the overlap time).
-        Also silently updates the maximum capacity, considering existing allocations
-        (again, worst case scenario : we consider the maximum free capacity if all allocations
-        are concurrent at some point in time)
-        """
+        for disk in node.disks:
+
+            worst_mean_disk_bw, overlap_duration = self.__compute_worst_disk_bw(disk, request)
+            disk.disk_status.bandwidth = worst_mean_disk_bw
+
+            # Remember that this disk has at least one running allocation that could
+            # compete for node bw
+            if overlap_duration != 0:
+                disks_overlaps.append((overlap_duration, disk.read_bandwidth))
+
+        nb_overlap_disks = len(disks_overlaps)
+        max_agg_bw = 0
+
+        for disk_data in disks_overlaps:
+            max_agg_bw += disk_data[1]  # sum bw of all active disks
+
+        if max_agg_bw < node.bandwidth:
+            # Node bw doen't throttle disks
+            worst_case_mean_node_bandwidth = node.bandwidth - max_agg_bw
+        else:
+            # Node bw may throttle disk. Each gets an equal part of the
+            # node network bandwidth, which may be > or < to the own disk
+            # bw, depending on the number of disks per node
+            worst_case_mean_node_bandwidth = node.bandwidth / nb_overlap_disks
+
+        node.node_status.bandwidth = worst_case_mean_node_bandwidth
+        # Throttle disk bandwidth according to node capacity
+        for disk in node.disks:
+            disk.disk_status.bandwidth = min(
+                disk.disk_status.bandwidth, worst_case_mean_node_bandwidth
+            )
+
+    def __compute_worst_disk_bw(self, disk, request):
+        """List all requests overlapping with our new request"""
 
         num_allocations = len(disk.allocations)
-        self.log.debug(f"[WC] .. This disk currently has {num_allocations} allocs")
-        overlap_offset = 0
-        node_bw = 0
-        disk_bw = 0
-        disk.disk_status.capacity = disk.capacity
+        self.log.debug(f"[WC_lo] .. This disk currently has {num_allocations} on-going allocations")
 
-        for idx, allocation in enumerate(disk.allocations):
+        overlap_durations = []
+        disk.disk_status.capacity = disk.capacity  # Reset 'status' value to full capacity
 
-            # Allocations are sorted on a per-disk basis upon insertion.
-            if allocation.end_time < request.start_time:
+        # Loop through allocations (from
+        for idx, allocation in enumerate(disk.allocations[::-1]):
+
+            # Note : when adding allocation on a disk, allocations are always
+            # sorted by end_time (from near future ending to ending in far future)
+            self.log.debug(f"[WC:disk] .. Allocation {idx}, end time : {allocation.end_time}")
+
+            if allocation.end_time <= request.start_time:
                 self.log.debug(
-                    f"[WC] .. Alloc {idx} ends at {allocation.end_time}."
+                    f"[WC:disk] .. Alloc {idx} ends at {allocation.end_time}."
                     + " That's before our request's allocation starts."
-                    + " Skipping this allocation"
+                    + " We can now stop"
                 )
-                continue
+                break
 
             # Allocations that may overlap with current request
             overlap_duration = request.overlaps(allocation)  # time in "seconds.microseconds"
             if overlap_duration != 0.0:
                 self.log.debug(
-                    f"[WC] .. Alloc {idx} and our request's alloc overlap for {overlap_duration}s"
-                )
-                overlap_requests = num_allocations - idx + 1
-                self.log.debug(
-                    f"[WC] .. Worst case, there are {overlap_requests} allocations "
-                    + "overlapping with our request"
+                    f"[WC:disk] .. Alloc {idx} and our request's alloc "
+                    + "overlap for {overlap_duration}s"
                 )
 
-                overlap_offset += overlap_duration  # formerly an update to start_time_chunk
-
-                node_bw += (overlap_duration * node.bandwidth) / overlap_requests
-                self.log.debug(f"[WC] .. Temp overlap node_bw={node_bw}")
-                disk_bw += (overlap_duration * disk.write_bandwidth) / overlap_requests
-                self.log.debug(f"[WC] .. Temp overlap disk_bw={disk_bw}")
+                overlap_durations.append(overlap_duration)
                 disk.disk_status.capacity -= allocation.capacity
-                self.log.debug(f"[WC] .. Disk capacity is {disk.disk_status.capacity}")
+                disk.disk_status.capacity = min(disk.disk_status.capacity, 0)
 
-        return (overlap_offset, node_bw, disk_bw)
+        self.log.debug(f"[WC:disk] We have {len(overlap_durations)} overlapping requests")
+        self.log.debug(f"[WC:disk] Overlap durations: {overlap_durations}")
+
+        if overlap_durations:
+            num_overlaps = len(overlap_durations) + 1  # +1 for our request
+            max_overlap_duration = max(overlap_durations)
+            self.log.debug(f"[WC:disk] Max overlap duration: {max_overlap_duration}")
+
+            request_duration_s = request.duration.total_seconds()
+
+            worst_case_mean_disk_bandwidth = (
+                (request_duration_s - max_overlap_duration) * disk.write_bandwidth
+                + (max_overlap_duration * (disk.write_bandwidth / num_overlaps))
+            ) / request_duration_s
+            worst_case_mean_disk_bandwidth = round(worst_case_mean_disk_bandwidth, 3)
+
+            self.log.debug(
+                f"[WC:disk] Mean worst_case disk bandwidth = {worst_case_mean_disk_bandwidth}"
+            )
+
+            return (worst_case_mean_disk_bandwidth, max_overlap_duration)
+
+        return (disk.write_bandwidth, 0)
